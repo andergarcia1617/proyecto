be28e8b94bfaaa7c35a8effb45c02a9eeb94207c
andergarcia1617 <34899284+andergarcia1617@users.noreply.github.com>
2024-11-27 11:12:24 -0500

    Primer commit de mi API

--- src/config.js
+++ src/config.js
@@ -7,3 +7,4 @@ export const DB_USER = process.env.DB_USER || 'root';
 export const DB_PASSWORD = process.env.DB_PASSWORD || '';
 export const DB_PORT = process.env.DB_PORT || 3306;
 export const PORT = process.env.PORT || 3000;
+const secret = process.env.JWT_SECRET || 'Garcia1920.';
